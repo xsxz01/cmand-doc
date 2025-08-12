9673801e501b1366c322aca8d90f26f3e14e6a35
Pang <pangyuyu@illusiontech.cn>
2025-08-12 10:19:25 +0800

    高级主题

--- docs/.vuepress/.temp/pages/advanced/index.html.js
+++ docs/.vuepress/.temp/pages/advanced/index.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Sheencity/Workspace/cmand-doc/docs/.vuepress/.temp/pages/advanced/index.html.vue"
-const data = JSON.parse("{\"path\":\"/advanced/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"高级主题\",\"slug\":\"高级主题\",\"link\":\"#高级主题\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"advanced/readme.md\"}")
+const data = JSON.parse("{\"path\":\"/advanced/\",\"title\":\"高级主题\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"高级主题\",\"sidebar\":false},\"headers\":[{\"level\":2,\"title\":\"高级主题\",\"slug\":\"高级主题\",\"link\":\"#高级主题\",\"children\":[]}],\"git\":{\"updatedTime\":1754964986000,\"contributors\":[{\"name\":\"Pang\",\"username\":\"Pang\",\"email\":\"pangyuyu@illusiontech.cn\",\"commits\":1,\"url\":\"https://github.com/Pang\"}],\"changelog\":[{\"hash\":\"6e6bedef3078ed9ef09906e2334b57833995ba14\",\"time\":1754964986000,\"email\":\"pangyuyu@illusiontech.cn\",\"author\":\"Pang\",\"message\":\"完成基本的文档内容框架（基础部分）\"}]},\"filePathRelative\":\"advanced/readme.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/advanced/readme.md
+++ docs/advanced/readme.md
@@ -1,1 +1,6 @@
+---
+title: "高级主题"
+sidebar: false
+
+---
 ## 高级主题
